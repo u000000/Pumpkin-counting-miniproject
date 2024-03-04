20c5028c45c1c011ec8872507cfb9786b1ececa2
oliver <oliver.torngaard@gmail.com>
2024-03-04 23:20:24 +0100

    tiles

--- Orthomosaic_count/Exercise3.4.py
+++ Orthomosaic_count/Exercise3.4.py
@@ -2,20 +2,49 @@ import cv2 as cv
 import rasterio
 from rasterio.plot import show
 from rasterio.windows import Window
+import os
 
-file = "ex.tif"
+
+path = os.path.dirname(__file__)
+
+file = os.path.join(path,"../othomosaics/pumkin_filed.tif")
+
+def get_tiles(file, tile_width, tile_height, overlap):
+    nols, nrows = file['width'], file['height']
+    xstep = tile_width - overlap
+    ystep = tile_height - overlap
+    for x in range(0, nols, xstep):
+        if x + tile_width > nols:
+            x = nols - tile_width
+        for y in range(0, nrows, ystep):
+            if y + tile_height > nrows:
+                y = nrows - tile_height
+            window = window.Window(x, y, tile_width, tile_height)
+            transform = window.transform(window, file.transform)
+            yield window, transform
+
+
 
 with rasterio.open(file) as O_file:
+
+    tile_x = O_file.width
+    tile_y = O_file.height
+    overlap = 50
+    metadata = O_file.meta.copy()
+
+
     col_start = 0
     row_start = 0
     col2 = O_file.width/2
     row2 = O_file.height/2
     col_end = O_file.width
     row_end = O_file.height   
-    window = Window.from_slices((row_start,row2,row_end), (col_start,col2,col_end))
+
+   #not done
+    window = Window.from_slices(slice(row2,row_end), slice(col2,col_end))
 
     data = O_file.read(window=window)
-
+    show(data)
  
 
 
