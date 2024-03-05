aa11e54e46c05dd4d0ca1eb8c170c4ab6546e0f9
Lucas R. Jeppesen <lucasjeppesen@gmail.com>
2024-03-05 23:05:17 +0100

    added class for splitting up to tiles

--- Orthomosaic_count/Exercise3.4.py
+++ Orthomosaic_count/Exercise3.4.py
@@ -1,50 +1,106 @@
 import cv2 as cv
+import numpy as np
 import rasterio
 from rasterio.plot import show
-from rasterio.windows import Window
+from rasterio.windows import Window,transform
+from rasterio import DatasetReader
 import os
-
-
-path = os.path.dirname(__file__)
-
-file = os.path.join(path,"../othomosaics/pumkin_filed.tif")
-
-def get_tiles(file, tile_width, tile_height, overlap):
-    nols, nrows = file['width'], file['height']
-    xstep = tile_width - overlap
-    ystep = tile_height - overlap
-    for x in range(0, nols, xstep):
-        if x + tile_width > nols:
-            x = nols - tile_width
-        for y in range(0, nrows, ystep):
-            if y + tile_height > nrows:
-                y = nrows - tile_height
-            window = window.Window(x, y, tile_width, tile_height)
-            transform = window.transform(window, file.transform)
-            yield window, transform
+from typing import Tuple
 
 
 
-with rasterio.open(file) as O_file:
+class tile_maneger:
 
-    tile_x = O_file.width
-    tile_y = O_file.height
-    overlap = 50
-    metadata = O_file.meta.copy()
+    def __init__(self,file_path, tile_width, tile_height, overlap) -> None:
+        self.file :DatasetReader = rasterio.open(file_path)
+        self.tile_width = tile_width
+        self.tile_height = tile_height
+        self.ncols, self.nrows = self.file.width, self.file.height
+        self.xstep = self.tile_width - overlap
+        self.ystep = self.tile_height - overlap
+        self.x = 0
+        self.y = 0
+        print(f"width:{self.ncols}  height:{self.nrows}")
+
+    def get_total_tile_count(self) -> int:
+        tiles_in_x = int(self.ncols / self.xstep)
+        if self.ncols % self.xstep != 0:
+            tiles_in_x = tiles_in_x + 1
+        tiles_in_y = int(self.nrows / self.ystep)
+        if self.nrows % self.ystep != 0:
+            tiles_in_y = tiles_in_y + 1    
+        return tiles_in_x*tiles_in_y
+    
+    def get_grid_shape(self) -> int:
+        tiles_in_x = int(self.ncols / self.xstep)
+        if self.ncols % self.xstep != 0:
+            tiles_in_x = tiles_in_x + 1
+        tiles_in_y = int(self.nrows / self.ystep)
+        if self.nrows % self.ystep != 0:
+            tiles_in_y = tiles_in_y + 1    
+        return (tiles_in_x,tiles_in_y)
+
+    def get_next_tile(self) -> Tuple[any,bool]:
+        is_last = False
+        if (self.y > self.nrows):
+                raise "no more tiles"
+        # print(f"x:{self.x}  y:{self.y}")
+
+        window = Window.from_slices(slice(self.y, self.y+self.tile_height),slice(self.x,self.x+self.tile_width))
+        # window_transform = transform(window, self.file.transform)
+        # print(window)
+        data = self.file.read(window=window,boundless=True)
 
 
-    col_start = 0
-    row_start = 0
-    col2 = O_file.width/2
-    row2 = O_file.height/2
-    col_end = O_file.width
-    row_end = O_file.height   
+        if (self.y + self.tile_height > self.nrows):
+            is_last = True
 
-   #not done
-    window = Window.from_slices(slice(row2,row_end), slice(col2,col_end))
+        if (self.x + self.xstep < self.ncols):
+            self.x = self.x + self.xstep
+        else:
+            self.x = 0
+            self.y = self.y + self.ystep
+        return data,is_last
+    
 
-    data = O_file.read(window=window)
-    show(data)
+if __name__ == "__main__" :
+
+    path = os.path.dirname(__file__)
+
+    file = os.path.join(path,"../othomosaics/pumkin_filed.tif")
+
+
+    img = tile_maneger(file,1000,1000,300)
+    print(img.get_total_tile_count())
+    for i in range(img.get_total_tile_count()-2):
+        [data,v] = img.get_next_tile()
+        img_array = np.transpose(data, (1, 2, 0))
+        img_array = cv.cvtColor(img_array,cv.COLOR_RGB2BGR)
+        cv.imshow("img",img_array)
+    [data,v] = img.get_next_tile()
+    print(f"x:{img.x}  y:{img.y} shape {img.get_grid_shape()}")
+    img_array = np.transpose(data, (1, 2, 0))
+    cv.imshow("img",img_array)
+    cv.waitKey(0)
+
+    #     tile_x = O_file.width
+    #     tile_y = O_file.height
+    #     overlap = 50
+    #     metadata = O_file.meta.copy()
+
+
+    #     col_start = 0
+    #     row_start = 0
+    #     col2 = O_file.width/2
+    #     row2 = O_file.height/2
+    #     col_end = O_file.width
+    #     row_end = O_file.height   
+
+    # # #not done
+    #     twindow = Window.from_slices(slice(row2,row_end), slice(col2,col_end))
+    #     print(twindow)
+
+        # data = O_file.read(window=window)
  
 
 
