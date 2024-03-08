b4fedb8b42333320f9733128c991ba37c7b6d080
u000000 <adasko99a@gmail.com>
2024-03-08 11:43:56 +0100

    a

--- Counting_objects/countPumpkin.py
+++ Counting_objects/countPumpkin.py
@@ -8,15 +8,15 @@ import sys
 path = os.path.dirname(__file__)
 photoName = sys.argv[1]
 img_BGR = cv2.imread(path+'/'+photoName)
-cv2.imshow('img', img_BGR)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# cv2.imshow('img', img_BGR)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
 
 # Convert image to CieLab
 img = cv2.cvtColor(img_BGR,cv2.COLOR_BGR2Lab)
-cv2.imshow('img_lab', img)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# cv2.imshow('img_lab', img)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
 
 # """# resize """
 # h, w, c = img.shape
@@ -55,40 +55,54 @@ mask = cv2.inRange(img, lower_pumpkin, upper_pumpkin)
 
 # Mask on original img
 img_masked = cv2.bitwise_and(img, img, mask=mask)
-cv2.imshow('img_masked', img_masked)
-cv2.waitKey()
-cv2.destroyAllWindows()
+# cv2.imshow('img_masked', img_masked)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
 
 """# filtering """
-img_dil = cv2.dilate(mask, np.ones((7, 7), np.uint8))
-cv2.imshow('img_dil', img_dil)
-cv2.waitKey()
-cv2.destroyAllWindows()
-img_ero = cv2.erode(img_dil, np.ones((5, 5), np.uint8))
-cv2.imshow('img_ero', img_ero)
-cv2.waitKey()
-cv2.destroyAllWindows()
+img_dil = cv2.dilate(mask, np.ones((1, 1), np.uint8))
+# cv2.imshow('img_dil', img_dil)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
+img_ero = cv2.erode(img_dil, np.ones((20, 20), np.uint8))
+# cv2.imshow('img_ero', img_ero)
+# cv2.waitKey()
+# cv2.destroyAllWindows()
 
 """# counting pumpkins """
-conts, hierarchy = cv2.findContours(img_ero, cv2.RETR_LIST ,cv2.CHAIN_APPROX_SIMPLE)
+conts, hierarchy = cv2.findContours(img_ero, cv2.RETR_TREE ,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(img_BGR, conts, -1, (255, 0, 0), 1)
 cv2.imshow('img_BGR', img_BGR)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
 number_of_pumpkins = 0
+height, width = img.shape[:2]
 centers = []
 for cont in conts:
     M = cv2.moments(cont)
     x0 = int(M['m10']/M['m00'])
     y0 = int(M['m01']/M['m00'])
     centers.append((x0, y0))
-    if cv2.contourArea(cont) > 900:
-        number_of_pumpkins += 2
-    else:
-        number_of_pumpkins += 1
+    partlyCutPumpkin = False
+
+    # for point in cont:
+    #     x, y = point[0]
+    #     print('x: ' + str(x))
+    #     print('y: ' + str(y))
+    #     if x == 0 or y == 0 or x == width or y == height:
+    #         partlyCutPumpkin = True
+    #     else:
+    #         continue
+    # if not partlyCutPumpkin:
+    #     number_of_pumpkins += 1
+    #     print('add')
 
 for center in centers:
     cv2.circle(img_BGR, center, 3, (0, 0, 255), 2)
+
+cv2.imshow('img_BGR', img_BGR)
+cv2.waitKey()
+cv2.destroyAllWindows()
 
 print('Number of pumpkins: ' + str(number_of_pumpkins))
