4f8be2af4cd0b00d1b76b5f61935b5e50cea135d
Lucas R. Jeppesen <lucasjeppesen@gmail.com>
2024-03-08 01:01:55 +0100

    test stuf

--- Orthomosaic_count/Exercise3.4.3_5.py
+++ Orthomosaic_count/Exercise3.4.3_5.py
@@ -84,9 +84,12 @@ class place_enum(Enum):
     
 
 
-def count_pumkins(img) -> int:
+def count_pumkins(img:cv.Mat,placement:place_enum,overlab) -> int:
 
     mean_color = np.array([225.69843634, 128.99106478, 176.34921817])
+
+    max_x = img.shape[0]
+    max_y = img.shape[1]
 
 
     img = cv.cvtColor(img,cv.COLOR_BGR2Lab)
@@ -104,29 +107,116 @@ def count_pumkins(img) -> int:
     img_dil = cv.dilate(img_ero, np.ones((7, 7), np.uint8))
  
 
-    cv.imshow("img",cv.cvtColor(img,cv.COLOR_Lab2BGR))
-    cv.imshow("mask",mask)
-    cv.imshow("img_dil",img_dil)
-    cv.imshow("img_ero",img_ero)
-    cv.waitKey(0)
+    # cv.imshow("img",cv.cvtColor(img,cv.COLOR_Lab2BGR))
+    # cv.imshow("mask",mask)
+    # cv.imshow("img_dil",img_dil)
+    # cv.imshow("img_ero",img_ero)
+    # cv.waitKey(0)
 
     """# counting pumpkins """
     conts, hierarchy = cv.findContours(img_dil, cv.RETR_LIST ,cv.CHAIN_APPROX_SIMPLE)
     cv.drawContours(img, conts, -1, (255, 0, 0), 1)
-
+    
     number_of_pumpkins = 0
-    centers = []
-    for cont in conts:
-        M = cv.moments(cont)
-        x0 = int(M['m10']/M['m00'])
-        y0 = int(M['m01']/M['m00'])
-        centers.append((x0, y0))
-        if cv.contourArea(cont) > 900:
-            number_of_pumpkins += 2
-        else:
-            number_of_pumpkins += 1
-
-    return number_of_pumpkins
+    match placement :
+        case place_enum.center:
+            return len(conts)
+        case place_enum.top_left:
+            for cont in conts:
+                M = cv.moments(cont)
+                x0 = int(M['m10']/M['m00'])
+                y0 = int(M['m01']/M['m00'])
+                if x0 > overlab and y0 > overlab:
+                    number_of_pumpkins += 1
+                elif x0 < overlab and y0 > overlab:
+                    number_of_pumpkins += x0/overlab
+                elif x0 > overlab and y0 < overlab:
+                    number_of_pumpkins += y0/overlab
+                elif x0 < overlab and y0 < overlab:
+                    number_of_pumpkins += sqrt(x0**2+y0**2)/sqrt((overlab**2)*2)
+            return number_of_pumpkins
+        case place_enum.top_right:
+            for cont in conts:
+                M = cv.moments(cont)
+                x0 = int(M['m10']/M['m00'])
+                y0 = int(M['m01']/M['m00'])
+                if max_x - x0 > overlab and y0 > overlab:
+                    number_of_pumpkins += 1
+                elif max_x - x0 < overlab and y0 > overlab:
+                    number_of_pumpkins += max_x - x0/overlab
+                elif max_x - x0 > overlab and y0 < overlab:
+                    number_of_pumpkins += y0/overlab
+                elif max_x - x0 < overlab and y0 < overlab:
+                    number_of_pumpkins += sqrt(max_x - x0**2+y0**2)/sqrt((overlab**2)*2)
+            return number_of_pumpkins
+        case place_enum.buttom_left:
+            for cont in conts:
+                M = cv.moments(cont)
+                x0 = int(M['m10']/M['m00'])
+                y0 = int(M['m01']/M['m00'])
+                if x0 > overlab and y0 > overlab:
+                    number_of_pumpkins += 1
+                elif x0 < overlab and y0 > overlab:
+                    number_of_pumpkins += x0/overlab
+                elif x0 > overlab and y0 < overlab:
+                    number_of_pumpkins += y0/overlab
+                elif x0 < overlab and y0 < overlab:
+                    number_of_pumpkins += sqrt(x0**2+y0**2)/sqrt((overlab**2)*2)
+            return number_of_pumpkins
+        case place_enum.buttom_right:
+            for cont in conts:
+                M = cv.moments(cont)
+                x0 = int(M['m10']/M['m00'])
+                y0 = int(M['m01']/M['m00'])
+                if max_x - x0 > overlab and max_y - y0 > overlab:
+                    number_of_pumpkins += 1
+                elif max_x - x0 < overlab and max_y - y0 > overlab:
+                    number_of_pumpkins += max_x - x0/overlab
+                elif max_x - x0 > overlab and max_y - y0 < overlab:
+                    number_of_pumpkins += max_y - y0/overlab
+                elif max_x - x0 < overlab and max_y - y0 < overlab:
+                    number_of_pumpkins += sqrt(max_x - x0**2+max_y - y0**2)/sqrt((overlab**2)*2)
+            return number_of_pumpkins
+        case place_enum.top:
+            for cont in conts:
+                M = cv.moments(cont)
+                x0 = int(M['m10']/M['m00'])
+                y0 = int(M['m01']/M['m00'])
+                if y0 > overlab:
+                    number_of_pumpkins += 1
+                elif y0 < overlab:
+                    number_of_pumpkins += y0/overlab
+            return number_of_pumpkins
+        case place_enum.left:
+            for cont in conts:
+                M = cv.moments(cont)
+                x0 = int(M['m10']/M['m00'])
+                y0 = int(M['m01']/M['m00'])
+                if x0 > overlab:
+                    number_of_pumpkins += 1
+                elif x0 < overlab:
+                    number_of_pumpkins += x0/overlab
+            return number_of_pumpkins
+        case place_enum.right:
+            for cont in conts:
+                M = cv.moments(cont)
+                x0 = int(M['m10']/M['m00'])
+                y0 = int(M['m01']/M['m00'])
+                if max_x - x0 > overlab:
+                    number_of_pumpkins += 1
+                elif max_x - x0 < overlab:
+                    number_of_pumpkins += x0/overlab
+            return number_of_pumpkins
+        case place_enum.buttom:
+            for cont in conts:
+                M = cv.moments(cont)
+                x0 = int(M['m10']/M['m00'])
+                y0 = int(M['m01']/M['m00'])
+                if max_y - y0 > overlab:
+                    number_of_pumpkins += 1
+                elif max_y - y0 < overlab:
+                    number_of_pumpkins += y0/overlab
+            return number_of_pumpkins
 
 if __name__ == "__main__" :
     
@@ -140,12 +230,12 @@ if __name__ == "__main__" :
     img_full = tile_maneger(file,1000,1000,20)
 
     while not last_tile:
-        [tile,last_tile] = img_full.get_next_tile()
+        [tile,last_tile,place] = img_full.get_next_tile()
 
         img_array = np.transpose(tile, (1, 2, 0))
         img = cv.cvtColor(img_array,cv.COLOR_RGB2BGR)
 
-        number_of_pumpkins += count_pumkins(img)
+        number_of_pumpkins += count_pumkins(img,place,20)
 
     print("num of pumpkins")
     print(number_of_pumpkins)
